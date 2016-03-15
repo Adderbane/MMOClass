95238accd79d2fe71aff1686faa62c8c7f4552e9
SADIKI SOLOMON (RIT Student) <sxs6590@ad.rit.edu>
2016-03-15 09:16:51 -0400

    Working on Start Menu

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Login : MonoBehaviour {
 
+    public Canvas canvas;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        //Debug.Log("Start!");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+    }
+
+    
+   /* void OnGUI()
+    {
+        GameObject.Find("Join Room").GetComponent<Canvas>().onClick.AddListener(() => { 
+            Debug.Log("Joining Room!"); });
+        GameObject.Find("Create Room").GetComponent<Button>().onClick.AddListener(() => { Debug.Log("Creating Room!"); });
+    }*/
+
+    public void JoinRoom()
+    {
+        Debug.Log("Joining Room!");
+        print("Test");
+    }
+
+    public void CreateRoom()
+    {
+        Debug.Log("Creating Room!");
+    }
 }
