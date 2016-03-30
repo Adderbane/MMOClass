fb70b5c56c2536c200ee8c5d501a28de1e5c50af
Dennis Tung <dxt298@gmail.com>
2016-03-30 19:24:28 -0400

    "Shooting"
    Clicking on a space on the screen now spawns a ball at that point.
    
    Still not hooked up to work online.

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class PlayerActions : MonoBehaviour {
 
+    public GameObject bulletPrefab;
+
     private int MAX_BULLETS = 1;
     private bool canShoot;
     private GameObject[] bullets;
     private int nextBullet;
     private float bulletSpeed;
+
+    private Ray ray;
+    private RaycastHit hit;
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +23,8 @@ public class PlayerActions : MonoBehaviour {
         nextBullet = 0;
         for (int i = 0; i < MAX_BULLETS; i++)
         {
-           // bullets[i] = (GameObject)Instantiate(bulletPrefab);
-           // bullets[i].SetActive(false);
+            bullets[i] = (GameObject)Instantiate(bulletPrefab);
+            bullets[i].SetActive(false);
         }
 	}
 
@@ -28,15 +33,21 @@ public class PlayerActions : MonoBehaviour {
     {
         if (Input.GetMouseButton(0))
         {
-            //GameObject temp = bullets[nextBullet++];
-            //if (nextBullet >= bullets.Length) nextBullet = 0;
-            //temp.SetActive(true);
-            //temp.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            //temp.transform.position = transform.position;
-            //temp.transform.rotation = transform.rotation;
-            //temp.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
-            Debug.Log("It's shooting");
+            //Vector2 screenCenterPoint = new Vector2(Screen.width / 2, Screen.height / 2);
+            //ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+            if (Physics.Raycast(ray, out hit, Camera.main.farClipPlane))
+            {
+                // A collision was detected please deal with it
+                Vector3 hitPosition = hit.point;
+                Debug.Log(hitPosition);
+                GameObject temp = bullets[nextBullet++];
+                if (nextBullet >= bullets.Length) nextBullet = 0;
+                temp.SetActive(true);
+                temp.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                temp.transform.position = hitPosition;
+            }
         }
 	}
 }
