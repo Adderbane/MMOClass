cf49a09b6ab9483fc1d5a43218e969055e92abc0
Sadiki <sxs6590@rit.edu>
2016-04-05 08:53:05 -0400

    Replaced Network Manager Hub with our own. The username input doesn't do anything right now.

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -1,32 +1,52 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Login : MonoBehaviour {
+public class Login : NetworkManager {
 
     public Canvas canvas;
 
-	// Use this for initialization
-	void Start () {
-    }
-	
-	// Update is called once per frame
-	void Update () {
-
+    public void StartHostGame()
+    {
+        // Set the network port then start hosting
+        SetPort();
+        NetworkManager.singleton.StartHost();
     }
 
-    public void JoinRoom(){
-        // Routes to the specified scene
-        Application.LoadLevel(1); // JoinRoom Scene
+    public void StartClientGame()
+    {
+        // Set the ip address and the network port then join the hosted game.
+        SetIPAddress();
+        SetPort();
+        NetworkManager.singleton.StartClient();
     }
 
-    public void CreateRoom(){
-        // Routes to the specified scene
-        Application.LoadLevel(2); // CreateRoom Scene
+    public void SetPort()
+    {
+        // Set the port to 7777
+        NetworkManager.singleton.networkPort = 7777;
     }
 
-    public void StartRoom(){
-       
-
+    public void SetIPAddress()
+    {
+        // Set the ip address to the entered value
+        string ipAddress = GameObject.Find("PortInput").transform.FindChild("Text").GetComponent<Text>().text;
+        NetworkManager.singleton.networkAddress = ipAddress;
     }
+
+    public void OnLevelWasLoaded(int level)
+    {
+        // Check if the game is in the lobby.
+        if (level == 0)
+        {
+            // Remove all active listeners of the type and then add a new listener.
+            GameObject.Find("JoinRoom").GetComponent<Button>().onClick.RemoveAllListeners();
+            GameObject.Find("JoinRoom").GetComponent<Button>().onClick.AddListener(StartClientGame);
+
+            GameObject.Find("CreateRoom").GetComponent<Button>().onClick.RemoveAllListeners();
+            GameObject.Find("CreateRoom").GetComponent<Button>().onClick.AddListener(StartHostGame);
+        }
+        
+    }
+
 }
