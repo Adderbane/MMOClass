b3801da0e448c9314c9ba1870dae8cf17c8ef693
Matthew Beer (RIT Student) <mab7799@ad.rit.edu>
2016-04-26 09:13:31 -0400

    Working on Explosion

---
+++ Assets/Scripts/Boom.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boom : MonoBehaviour {
+
+    public float explosionForce = 1000;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    //Collision
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.gameObject.GetComponent<Rigidbody>().AddExplosionForce(1000, transform.position, 2.5f, 3.0f);
+        }
+    }
+}
