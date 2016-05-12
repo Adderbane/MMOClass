5909a7696d0dd68a72b05a98ba762e57cac2e423
Matthew Beer (RIT Student) <mab7799@ad.rit.edu>
2016-05-12 09:01:32 -0400

    Fixed player syncing bug

--- Assets/Scripts/PlayerNetSetup.cs
+++ Assets/Scripts/PlayerNetSetup.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class PlayerNetSetup : NetworkBehaviour {
 
@@ -14,6 +15,7 @@ public class PlayerNetSetup : NetworkBehaviour {
         {
             cam.enabled = true;
             audioList.enabled = true;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>().enabled = true;
         }
 
 	}
